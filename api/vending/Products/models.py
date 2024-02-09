fcc5201ef9a940678c8c7da34515bbca8fb138a6
Peter Howe <pnhowe@gmail.com>
2024-02-08 22:36:03 -0700

    Nates UI can now talk to the api

--- api/vending/Products/models.py
+++ api/vending/Products/models.py
@@ -9,9 +9,9 @@ cinp = CInP( 'Products', '0.1' )
 @cinp.model()
 class Product( models.Model ):
   name = models.CharField( max_length=200 )
-  cost = models.IntegerField( min_value=0, help='in howe-bucks' )
+  cost = models.IntegerField( help_text='in howe-bucks' )
   location = models.CharField( max_length=3 )
-  available = models.IntegerField( min_value=0 )
+  available = models.IntegerField()
   
   updated = models.DateTimeField( editable=False, auto_now=True )
   created = models.DateTimeField( editable=False, auto_now_add=True )
@@ -20,17 +20,23 @@ class Product( models.Model ):
   @cinp.check_auth()
   @staticmethod
   def checkAuth( user, verb, id_list, action=None ):
-    return cinp.basic_auth_check( user, verb, action, Job )
+    return cinp.basic_auth_check( user, verb, action, Product )
 
   def clean( self, *args, **kwargs ):  # TODO: also need to make sure a Structure is in only one complex
     super().clean( *args, **kwargs )
     errors = {}
+    
+    if self.cost < 0:
+      errors[ 'cost' ] = 'Must be at least 0'
+
+    if self.cost < 0:
+      errors[ 'available' ] = 'Must be at least 0'
 
     if errors:
       raise ValidationError( errors )
 
-  class Meta:
-    default_permissions = ()
+  # class Meta:
+  #   default_permissions = ()
 
   def __str__( self ):
     return 'Product "{0}"'.format( self.name )
