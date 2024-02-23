c53b8f094f111be4318afe6ec67f26efcf4b1746
NateTheGreat <nathanprattwashburn@gmail.com>
2024-02-23 11:53:31 -0700

    added getPrice function and async function for get products array.

--- GUI-Nate/script.js
+++ GUI-Nate/script.js
@@ -1,18 +1,14 @@
 const cinp = cinpBuilder( "http://localhost:8888" );
 
 function setupBackend() {
-
   cinp.describe( "/api/v1/" ).done( 
     function( resp ) {
       if( resp.version != "1.0" ) {
         alert( "Invalid API version" );
         return;
       }
-      
       login();
     } ).fail( function() { alert( "unable to find backend" ); } );
-
-
 }
 
 function login() {
@@ -30,7 +26,6 @@ function doAlert( msg ) {
   alert( msg );
 }
 
-
 function test() {  
   const product_list = $("#product_list");
   product_list.empty();
@@ -42,6 +37,43 @@ function test() {
     } ).fail( function() {
       alert( "Unable to get product list" );
     } );
+}
+
+function getProductsArr() {
+  return new Promise((resolve, reject) => {
+    let products = [];
+    cinp.getFilteredObjects("/api/v1/Products/Product").done(function(resp) {
+      for (const [uri, product] of Object.entries(resp)) {
+        let productArray = [
+          product.name,
+          product.cost,
+          product.location,
+          product.available,
+          product.id
+        ];
+        products.push(productArray);
+      }
+      resolve(products);
+    }).fail(function() {
+      reject("Unable to get product list");
+    });
+  });
+}
+
+async function showPrice(button) {
+  try {
+    let products = await getProductsArr();
+    const productLocation = button.textContent;
+    console.log(productLocation + "|" + products.length);
+    for (let i = 0; i < products.length; i++) {
+      console.log(products[i][2]);
+      if (products[i][2] === productLocation) {
+        alert("The cost is $" + products[i][1]);
+      }
+    }
+  } catch (error) {
+    alert(error);
+  }
 }
 
 function showAlert(button) {
